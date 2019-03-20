07038a668bd141d459a7c590fca7bd5b4305527f
David Kerr <lotophagos@live.co.uk>
2019-03-20 16:57:43 +0000

    Completed tutorial: Get started with Razor Pages in ASP.NET Core

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                // look for any movies
+                if (!context.Movie.Any())
+                {
+                    context.Movie.AddRange(
+                        new Movie
+                        {
+                            Title = "When Harry Met Sally",
+                            ReleaseDate = DateTime.Parse("12/2/1989"),
+                            Genre = "Romantic Comedy",
+                            Price = 7.99M,
+                            Rating = "R"
+                        },
+                        new Movie
+                        {
+                            Title = "Ghostbusters",
+                            ReleaseDate = DateTime.Parse("13/3/1984"),
+                            Genre = "Comedy",
+                            Price = 8.99M,
+                            Rating = "R"
+                        },
+                        new Movie
+                        {
+                            Title = "Ghostbusters 2",
+                            ReleaseDate = DateTime.Parse("23/2/1986"),
+                            Genre = "Comedy",
+                            Price = 9.99M,
+                            Rating = "R"
+                        },
+                        new Movie
+                        {
+                            Title="Rio Bravo",
+                            ReleaseDate=DateTime.Parse("15/4/1954"),
+                            Genre="Western",
+                            Price=3.99M,
+                            Rating="R"
+                        }
+                        );
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie
 {
@@ -14,11 +17,34 @@ namespace RazorPagesMovie
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            SeedDB(host);
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        public static void SeedDB(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<RazorPagesMovieContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception exc)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exc, "An error occurred seeding the DB");
+                }
+            }
+        }
     }
 }
